a6581c81dc00de147de3a9ed141a34b23e77545a
zhangwenming <wenming.zhang@sony.com>
2017-09-07 09:40:57 +0800

    update

--- column.cpp
+++ column.cpp
@@ -6,12 +6,12 @@ column_window::column_window(unsigned int column_num, double max_value, int widt
 	int column_base = width / (column_num * 3 + 1);
 	this->column_interval = column_base;
 	this->column_width = column_base * 2;
-	this->bottom_height = 40;
+	this->bottom_height = 60;
 	this->column_height = this->height - this->bottom_height;
 
 	for (unsigned int i = 0; i < this->column_num; ++i){
 		std::pair<int, int> coordinate;
-		coordinate.first = column_base * (3 * i + 1) ;
+		coordinate.first = column_base * (3 * i + 1);
 		coordinate.second = coordinate.first + column_base * 2;
 		this->column_horizontal_coordinates.push_back(coordinate);
 	}
@@ -30,21 +30,28 @@ bool column_window::update_data(std::vector<double> datalist){
 		return false;
 	int vertical_coordinate_bottom = this->column_height;
 
+    cv::rectangle(this->mat, cv::Point(0, this->height - 30), cv::Point(this->width, this->height - this->bottom_height), cv::Scalar(255, 255, 255), -1);
 	for (unsigned int i = 0; i < this->column_num; ++i){
 		cv::rectangle(this->mat, cv::Point(this->column_horizontal_coordinates[i].first, vertical_coordinate_bottom),
 			cv::Point(this->column_horizontal_coordinates[i].second, int(vertical_coordinate_bottom * (1 - datalist[i]))), cv::Scalar(50, 200, 50), -1);
 		cv::rectangle(this->mat, cv::Point(this->column_horizontal_coordinates[i].first, 0),
 			cv::Point(this->column_horizontal_coordinates[i].second, int(vertical_coordinate_bottom * (1 - datalist[i]))), cv::Scalar(200, 200, 200), -1);
+        cv::putText(this->mat, std::to_string(datalist[i]),
+            cv::Point(this->column_horizontal_coordinates[i].first, this->height - 40), cv::FONT_HERSHEY_COMPLEX, 0.4, cv::Scalar(0, 0, 0), 1, CV_AA);
 	}
 	cv::imshow(this->window_name, this->mat);
 	return true;
 }
 
 void column_window::show_framework(){
+    std::string string_G = "G";
+
+    cv::line(this->mat, cv::Point(0, this->column_height), cv::Point(this->width, this->column_height), cv::Scalar(255, 255,255));
+
 	for (unsigned int i = 0; i < this->column_num; ++i){
-		int text_column_start = this->column_horizontal_coordinates[i].first;
-		int text_row_start = this->height - 10;
-		cv::putText(this->mat, std::to_string(i + 1), cv::Point(text_column_start, text_row_start), cv::FONT_HERSHEY_COMPLEX, 0.5, cv::Scalar(0, 0, 0), 1, CV_AA);
+		cv::putText(this->mat, string_G + std::to_string(i + 1),
+            cv::Point(this->column_horizontal_coordinates[i].first, this->height - 10),
+            cv::FONT_HERSHEY_COMPLEX, 0.4, cv::Scalar(0, 0, 0), 1, CV_AA);
 	}
     cv::imshow(this->window_name, this->mat);
 }
